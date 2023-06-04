8dfc917abdd4054e324165af2ab539172ce4b027
aytanbayramova <112419467+aytanbayramova@users.noreply.github.com>
2023-06-04 23:54:22 +0400

    To do task is ready and coded with angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,96 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { TodoAddEditComponent } from './todo-add-edit/todo-add-edit.component';
+import { TodoService } from './service/todo.service';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import { CoreService } from './core/core.service';
+import {MatTableDataSource} from '@angular/material/table';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  displayedColumns: string[] = [
+    'id',
+    'todo',
+     'whattimeisit',
+     'action'
+    ];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  title: any;
+
+
+  constructor( 
+    private _dialog: MatDialog, 
+    private _todoService:TodoService,
+    private _coreService: CoreService
+
+    ){}
+
+    ngOnInit(): void {
+      this.getTodoList();
+    }
+
+    openAddEditTodoForm() {
+      const dialogRef = this._dialog.open(TodoAddEditComponent);
+      dialogRef.afterClosed().subscribe({
+        next: (val) => {
+          if (val) {
+            this.getTodoList();
+          }
+        },
+      });
+    }
+
+  getTodoList(){
+    this._todoService.getTaskList().subscribe({
+      next: (res) =>{
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      },
+      error: console.log,
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+
+  deleteTask (id:number){
+    this._todoService.deleteTask(id).subscribe({
+      next: (res) => {
+        this._coreService.openSnackBar('Task deleted!', 'done');
+        this.getTodoList();
+      },
+      error: console.log
+    })
+  }
+
+  openEditTodoForm(data:any){
+   const dialogRef= this._dialog.open(TodoAddEditComponent, {
+      data,
+    } ) ;
+
+    dialogRef.afterClosed().subscribe({
+      next: (val) =>{
+        if (val){
+          this.getTodoList();
+        }
+      },
+    });
+     
+  }
+}
+
